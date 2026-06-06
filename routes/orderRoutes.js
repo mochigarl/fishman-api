133c9084d9753cca0cd64b9e2b4b3fd96be196e8
shiffaazaharrr <shfazhr03@gmail.com>
2026-06-06 23:57:42 +0800

    Update orderRoutes.js

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -10,9 +10,9 @@ const {
 } = require("../controllers/orderController")
 
 router.get("/", getOrders)
-router.get("/customer/:phone", getOrdersByPhone)
 router.post("/", addOrder)
 router.put("/:id", updateOrderStatus)
 router.delete("/:id", deleteOrder)
+router.get("/phone/:phone", getOrdersByPhone)
 
 module.exports = router
